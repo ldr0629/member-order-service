0d9af925f0359c0367b2ca6d3e4ebee77831d96c
ldr0629 <leedero01@naver.com>
2022-03-03 22:31:37 +0900

    회원, 주문 서비스 기능 테스트

---
+++ src/main/java/basic/core/OrderApp.java
@@ -0,0 +1,24 @@
+package basic.core;
+
+import basic.core.member.Grade;
+import basic.core.member.Member;
+import basic.core.member.MemberService;
+import basic.core.member.MemberServiceImpl;
+import basic.core.order.Order;
+import basic.core.order.OrderService;
+import basic.core.order.OrderServiceImpl;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+    }
+}
