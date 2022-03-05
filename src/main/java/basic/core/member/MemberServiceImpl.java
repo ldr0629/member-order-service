0170b1163ab2027e2f3f05f22ee3bf2f546aef1b
ldr0629 <leedero01@naver.com>
2022-03-06 02:35:56 +0900

    IoC, DI 적용

--- src/main/java/basic/core/member/MemberServiceImpl.java
+++ src/main/java/basic/core/member/MemberServiceImpl.java
@@ -2,7 +2,14 @@ package basic.core.member;
 
 public class MemberServiceImpl implements MemberService{
     // 추상화뿐만 아니라 구현체에도 의존하는 문제 발생. DIP 원칙 위반
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 추상화에만 의존
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
