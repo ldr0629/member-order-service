c89d43146f676eb6c5bfb2176ef1fa46c8a174ed
ldr0629 <leedero01@naver.com>
2022-03-06 02:33:08 +0900

    스프링 컨테이너 설정 및 DIP, OCP 위반 해결

---
+++ src/main/java/basic/core/AppConfig.java
@@ -0,0 +1,38 @@
+package basic.core;
+
+import basic.core.discount.DiscountPolicy;
+import basic.core.discount.RateDiscountPolicy;
+import basic.core.member.MemberRepository;
+import basic.core.member.MemberService;
+import basic.core.member.MemberServiceImpl;
+import basic.core.member.MemoryMemberRepository;
+import basic.core.order.OrderService;
+import basic.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// 구현 객체 생성과, 생성자를 통한 주입
+// 애플리케이션의 구성정보를 담당하는 애노테이션, 스프링 컨테이너에 객체 등록
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
+    }
+}
