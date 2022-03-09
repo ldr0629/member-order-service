4abd70dbd4b1cf6f19285d22b1216b037b48829f
ldr0629 <leedero01@naver.com>
2022-03-09 23:13:12 +0900

    컴포넌트 스캔 테스트

---
+++ src/test/java/basic/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,19 @@
+package basic.core.scan;
+
+
+import basic.core.AutoAppConfig;
+import basic.core.member.Member;
+import basic.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AutoAppConfigTest {
+    @Test
+    void basicScan() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
