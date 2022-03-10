eabd7b7a2b57a5ae9707e61afbfff4d6ae0f9bcb
ldr0629 <leedero01@naver.com>
2022-03-11 03:58:38 +0900

    생성자 주입 사용

---
+++ src/test/java/basic/core/order/OrderServiceImplTest.java
@@ -0,0 +1,22 @@
+package basic.core.order;
+
+import basic.core.discount.FixDiscountPolicy;
+import basic.core.member.Grade;
+import basic.core.member.Member;
+import basic.core.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class OrderServiceImplTest {
+    @Test
+    void createOrder() {
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
