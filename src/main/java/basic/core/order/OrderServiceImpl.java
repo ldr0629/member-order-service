bff46a1709ead39f381e452c76e742a86b1bfb69
ldr0629 <leedero01@naver.com>
2022-03-11 03:57:17 +0900

    의존관계 주입 방식 파악

--- src/main/java/basic/core/order/OrderServiceImpl.java
+++ src/main/java/basic/core/order/OrderServiceImpl.java
@@ -3,20 +3,39 @@ package basic.core.order;
 import basic.core.discount.DiscountPolicy;
 import basic.core.member.Member;
 import basic.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor// final이 붙은 변수들을 가지고 자동으로 생성자를 만들어준다. (롬복 사용)
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+//    @Autowired private MemberRepository memberRepository; (필드 주입)
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired (수정자 주입)
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+
+//    @Autowired // (일반 메서드 주입)
+//    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
+
+//    @Autowired // (생성자 주입, @Autowired 생략 가능)
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
