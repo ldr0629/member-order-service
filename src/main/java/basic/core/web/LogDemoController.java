6bb2ac7c6b9ce447bf69ea05109059ce8b5a94b6
ldr0629 <leedero01@naver.com>
2022-03-14 18:30:21 +0900

    Provider를 이용한 request scope의 log 확인

---
+++ src/main/java/basic/core/web/LogDemoController.java
@@ -0,0 +1,30 @@
+package basic.core.web;
+
+import basic.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+    private final LogDemoService logDemoService;
+//    private final MyLogger myLogger; 초기 앱 실행시 request scope 내에 없기때문에 에러 발생
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject(); // http 요청이 진행중이므로 request scope 빈 생성이 정상처리
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
